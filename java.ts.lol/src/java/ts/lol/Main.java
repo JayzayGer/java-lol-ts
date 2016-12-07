8b5b07ddf085484267a03c5cf50f0e96ab47b264
SGanser <Jayzay-Sebb@web.de>
2016-12-07 12:24:24 +0100

    -added required libs -added start class

---
+++ java.ts.lol/src/java/ts/lol/Main.java
@@ -0,0 +1,9 @@
+package java.ts.lol;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new UIEntryPoint().start();
+	}
+
+}
---
+++ java.ts.lol/src/java/ts/lol/UIEntryPoint.java
@@ -0,0 +1,36 @@
+package java.ts.lol;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class UIEntryPoint {
+	
+	
+	private Shell shell;
+
+
+	public void start(){
+		shell = new Shell(Display.getCurrent());
+		shell.setBackgroundMode(SWT.INHERIT_FORCE);
+		shell.setLayout(new FillLayout());
+
+		
+		createContent();
+
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!shell.getDisplay().readAndDispatch()) {
+				shell.getDisplay().sleep();
+			}
+		}
+		Display.getCurrent().dispose();
+	}
+	
+	
+	private void createContent(){
+		
+	}
+
+}
